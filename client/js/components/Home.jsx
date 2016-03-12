03ab9ff3f872f0ea218ec0af4efca107fb394c1a
Grant Forrest <a-type@outlook.com>
2016-03-12 15:12:26 -0500

    Tweak idle time and disable transitions for weak PI processor

--- client/js/components/Home.jsx
+++ client/js/components/Home.jsx
@@ -8,7 +8,7 @@ const moment = require('moment');
 const numBackgrounds = 20;
 const backgroundChangeInterval = 60 * 60 * 1000;
 const weatherUpdateInterval = 60 * 60 * 1000;
-const idleTimeout = 5 * 60 * 1000;
+const idleTimeout = 10 * 1000;
 
 const getTime = () => {
     return moment().format('h:mm a');
@@ -30,79 +30,79 @@ export default class Home extends Component {
             idleBackground: getRandomBackground()
         };
     }
-    
+
     fetchWeather () {
         let self = this;
-        
+
         request.get('/weather', (er, response) => {
             self.setState({
                 forecast: JSON.parse(response.body)
             });
         });
     }
-    
+
     fetchUser () {
         let self = this;
-        
+
         request.get('/user', (er, response) => {
             self.setState({
                 user: JSON.parse(response.body)
             });
         });
     }
-    
+
     goIdle () {
         this.setState({
             idleBackground: getRandomBackground(),
             idle: true
         });
     }
-    
+
     componentDidMount () {
         this.fetchUser();
-        
+
         let self = this;
-        
+
         window.addEventListener('keydown', (e) => {
             if (e.code === 'KeyA' || e.char === 'a' || e.key === 'a' || e.keyCode === 65) {
                 self.setState({ idle: false });
-                
+
                 setTimeout(self.goIdle, idleTimeout);
             }
             else if (e.code === 'KeyI' || e.char === 'i' || e.key === 'i' || e.keyCode === 75) {
                 self.goIdle();
             }
         });
-        
+
         (function updateTime() {
             self.setState({ time: getTime() });
-            
+
             setTimeout(updateTime, 60 * 1000);
         })();
-        
+
         (function nextBackground() {
             self.setState({ idleBackground: getRandomBackground() });
-            
+
             setTimeout(nextBackground, backgroundChangeInterval);
         })();
-        
+
         (function updateWeather() {
             self.fetchWeather();
-            
+
             setTimeout(updateWeather, weatherUpdateInterval);
         })();
     }
-    
+
     render () {
         let className = 'home';
         if (this.state.idle) {
             className += ' home-idle';
         }
-        
+
         const idleBackgroundStyle = {
             //backgroundImage: this.state.idleBackground
         };
-        
+
         return (
             <div className={className}>
                 <div className='info'>
