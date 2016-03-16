f5f27f9fe683da214c15ce4f33e77ba4af421d81
Grant Forrest <a-type@outlook.com>
2016-03-16 19:40:57 -0400

    Remove idle

--- client/js/components/Home.jsx
+++ client/js/components/Home.jsx
@@ -5,18 +5,10 @@ import Weather from './Weather';
 const request = require('browser-request');
 const moment = require('moment');
 
-const numBackgrounds = 20;
-const backgroundChangeInterval = 60 * 60 * 1000;
 const weatherUpdateInterval = 60 * 60 * 1000;
-const idleTimeout = 10 * 1000;
 
 const getTime = () => {
     return moment().format('h:mm a');
-}
-
-const getRandomBackground = () => {
-    const idx = Math.floor(Math.random() * numBackgrounds);
-    return `url(/img/idle/${idx}.jpg)`;
 }
 
 export default class Home extends Component {
@@ -25,7 +17,6 @@ export default class Home extends Component {
         this.state = {
             user: { name: '' },
             forecast: null,
-            idle: true,
             time: getTime(),
             idleBackground: getRandomBackground()
         };
@@ -51,39 +42,15 @@ export default class Home extends Component {
         });
     }
 
-    goIdle () {
-        this.setState({
-            idleBackground: getRandomBackground(),
-            idle: true
-        });
-    }
-
     componentDidMount () {
         this.fetchUser();
 
         let self = this;
 
-        window.addEventListener('keydown', (e) => {
-            if (e.code === 'KeyA' || e.char === 'a' || e.key === 'a' || e.keyCode === 65) {
-                self.setState({ idle: false });
-
-                setTimeout(self.goIdle, idleTimeout);
-            }
-            else if (e.code === 'KeyI' || e.char === 'i' || e.key === 'i' || e.keyCode === 75) {
-                self.goIdle();
-            }
-        });
-
         (function updateTime() {
             self.setState({ time: getTime() });
 
             setTimeout(updateTime, 60 * 1000);
-        })();
-
-        (function nextBackground() {
-            self.setState({ idleBackground: getRandomBackground() });
-
-            setTimeout(nextBackground, backgroundChangeInterval);
         })();
 
         (function updateWeather() {
@@ -95,22 +62,12 @@ export default class Home extends Component {
 
     render () {
         let className = 'home';
-        if (this.state.idle) {
-            className += ' home-idle';
-        }
-
-        const idleBackgroundStyle = {
-            //backgroundImage: this.state.idleBackground
-        };
 
         return (
             <div className={className}>
                 <div className='info'>
                     <Greeting user={this.state.user}/>
                     <Weather forecast={this.state.forecast}/>
-                </div>
-                <div className='idle' style={idleBackgroundStyle}>
-                    <h1 className='idle-time'>{this.state.time}</h1>
                 </div>
             </div>
         );
